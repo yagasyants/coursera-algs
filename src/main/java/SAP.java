7c218d03a3affaf1556a5c6a0a616e0685765188
yagasyants <yagasyants@gmail.com>
2014-04-08 08:17:31 -0700

    fix checkstyle issues

--- src/main/java/SAP.java
+++ src/main/java/SAP.java
@@ -1,62 +1,75 @@
 import java.util.Arrays;
 
 public class SAP {
-	private static final int INFINITY = Integer.MAX_VALUE;
-	private static final int UNKNOWN = -1;
+    private static final int INFINITY = Integer.MAX_VALUE;
+    private static final int UNKNOWN = -1;
 
-	private Digraph digraph;
+    private Digraph digraph;
 
-	public SAP(Digraph digraph) {
-		this.digraph = digraph;
-	}
+    public SAP(Digraph digraph) {
+        this.digraph = clone(digraph);
+    }
 
-	public int length(int v, int w) {
-		return length(Arrays.asList(v), Arrays.asList(w));
-	}
+    private Digraph clone(Digraph digraphToClone) {
+        Digraph clone = new Digraph(digraphToClone.V());
+        for (int v = 0; v < clone.V(); v++) {
+            Iterable<Integer> vertices = digraphToClone.adj(v);
+            for (int w : vertices) {
+                clone.addEdge(v, w);
+            }
+        }
+        return clone;
+    }
 
-	public int ancestor(int v, int w) {
-		return ancestor(Arrays.asList(v), Arrays.asList(w));
-	}
+    public int length(int v, int w) {
+        return length(Arrays.asList(v), Arrays.asList(w));
+    }
 
-	public int length(Iterable<Integer> v, Iterable<Integer> w) {
-		return findSAP(v, w)[1];
-	}
+    public int ancestor(int v, int w) {
+        return ancestor(Arrays.asList(v), Arrays.asList(w));
+    }
 
-	public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-		return findSAP(v, w)[0];
-	}
+    public int length(Iterable<Integer> v, Iterable<Integer> w) {
+        return findSAP(v, w)[1];
+    }
 
-	private int[] findSAP(Iterable<Integer> v, Iterable<Integer> w) {
-		BreadthFirstDirectedPaths bfsV = new BreadthFirstDirectedPaths(digraph, v);
-		BreadthFirstDirectedPaths bfsW = new BreadthFirstDirectedPaths(digraph, w);
+    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+        return findSAP(v, w)[0];
+    }
 
-		int[] sapAncLength = { INFINITY, INFINITY };
+    private int[] findSAP(Iterable<Integer> v, Iterable<Integer> w) {
+        BreadthFirstDirectedPaths bfsV = new BreadthFirstDirectedPaths(digraph,
+                v);
+        BreadthFirstDirectedPaths bfsW = new BreadthFirstDirectedPaths(digraph,
+                w);
 
-		for (int i = 0; i < digraph.V(); i++) {
-			int distFromIToV = bfsV.distTo(i);
-			int distFromIToW = bfsW.distTo(i);
+        int[] sapAncLength = { INFINITY, INFINITY };
 
-			if (distFromIToV != INFINITY && distFromIToW != INFINITY) {
-				int ancPathLength = distFromIToV + distFromIToW;
-				if (sapAncLength[1] == INFINITY || ancPathLength < sapAncLength[1]) {
-					sapAncLength[0] = i;
-					sapAncLength[1] = ancPathLength;
-				}
+        for (int i = 0; i < digraph.V(); i++) {
+            int distFromIToV = bfsV.distTo(i);
+            int distFromIToW = bfsW.distTo(i);
 
-			}
-		}
+            if (distFromIToV != INFINITY && distFromIToW != INFINITY) {
+                int ancPathLength = distFromIToV + distFromIToW;
+                if (sapAncLength[1] == INFINITY
+                        || ancPathLength < sapAncLength[1]) {
+                    sapAncLength[0] = i;
+                    sapAncLength[1] = ancPathLength;
+                }
 
-		return convertToUnknownIfInfinity(sapAncLength);
-	}
-	
-	private int[] convertToUnknownIfInfinity(int[] sapAncLength){
-		if(sapAncLength[0] == INFINITY){
-			int[] unknown = { UNKNOWN, UNKNOWN };
-			return unknown;
-		}else{
-			return sapAncLength;
-		}
-	}
+            }
+        }
 
+        return convertToUnknownIfInfinity(sapAncLength);
+    }
+
+    private int[] convertToUnknownIfInfinity(int[] sapAncLength) {
+        if (sapAncLength[0] == INFINITY) {
+            int[] unknown = { UNKNOWN, UNKNOWN };
+            return unknown;
+        } else {
+            return sapAncLength;
+        }
+    }
 
 }
--- src/main/java/WordNet.java
+++ src/main/java/WordNet.java
@@ -1,7 +1,3 @@
-
-
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -9,188 +5,187 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-
 public class WordNet {
-	private static final Integer UNKNOWN_ROOT = Integer.MIN_VALUE;
+    private static final int UNKNOWN_ROOT = Integer.MIN_VALUE;
 
-	private Map<String, List<Integer>> nounToVertex = new HashMap<>();
-	private List<String> synsets = new ArrayList<>();
-	private int root = UNKNOWN_ROOT;
-	private Digraph digraph;
+    private Map<String, List<Integer>> nounToVertex = new HashMap<>();
+    private List<String> synsets = new ArrayList<>();
+    private int root = UNKNOWN_ROOT;
+    private Digraph digraph;
 
-	public WordNet(String synFile, String hypFile) {
-		try {
-			processFile(synFile, new SynFileProcessor());
-			processFile(hypFile, new HypFileProcessor());
-		} catch (IOException e) {
-			throw new IllegalArgumentException(e);
-		}
+    public WordNet(String synFile, String hypFile) {
+        processFile(synFile, new SynFileProcessor());
+        processFile(hypFile, new HypFileProcessor());
 
-		validatGraphIsRootedDAG();
-	}
+        validatGraphIsRootedDAG();
+    }
 
-	private void processFile(String synFile, FileProcessor fileProcessor) throws IOException {
-		In in = null;
-		try {
-			in = new In(new File(synFile));
+    private void processFile(String synFile, FileProcessor fileProcessor) {
+        In in = null;
+        try {
+            in = new In(synFile);
 
-			String strLine;
-			int lineNumber = 0;
-			while ((strLine = in.readLine()) != null) {
-				fileProcessor.processLine(strLine, lineNumber);
+            String strLine;
+            int lineNumber = 0;
+            while ((strLine = in.readLine()) != null) {
+                fileProcessor.processLine(strLine, lineNumber);
 
-				lineNumber++;
-			}
-			fileProcessor.postLineProcess(lineNumber);
-		} finally {
-			close(in);
-		}
-	}
+                lineNumber++;
+            }
+            fileProcessor.postLineProcess(lineNumber);
+        } finally {
+            close(in);
+        }
+    }
 
-	private void close(In closable) throws IOException {
-		if (closable != null) {
-			closable.close();
-		}
-	}
+    private void close(In closable) {
+        if (closable != null) {
+            closable.close();
+        }
+    }
 
-	private interface FileProcessor {
-		void processLine(String strLine, int lineNumber);
+    private interface FileProcessor {
+        void processLine(String strLine, int lineNumber);
 
-		void postLineProcess(int processedLines);
-	}
+        void postLineProcess(int processedLines);
+    }
 
-	private class SynFileProcessor implements FileProcessor {
+    private class SynFileProcessor implements FileProcessor {
 
-		@Override
-		public void processLine(String strLine, int lineNumber) {
-			String[] parts = strLine.split(",");
-			validateParts(strLine, parts, 2);
-			String idStr = parts[0];
-			String nounsStr = parts[1];
-			validate(idStr, strLine, lineNumber);
-			synsets.add(nounsStr);
+        @Override
+        public void processLine(String strLine, int lineNumber) {
+            String[] parts = strLine.split(",");
+            validateParts(strLine, parts, 2);
+            String idStr = parts[0];
+            String nounsStr = parts[1];
+            validate(idStr, strLine, lineNumber);
+            synsets.add(nounsStr);
 
-			addNouns(lineNumber, nounsStr);
-		}
+            addNouns(lineNumber, nounsStr);
+        }
 
-		@Override
-		public void postLineProcess(int processedLines) {
-			digraph = new Digraph(processedLines);
-		}
-	}
+        @Override
+        public void postLineProcess(int processedLines) {
+            digraph = new Digraph(processedLines);
+        }
+    }
 
-	private class HypFileProcessor implements FileProcessor {
-		@Override
-		public void processLine(String strLine, int lineNumber) {
-			String[] arrayIds = strLine.split(",");
-			validateParts(strLine, arrayIds, 1);
-			int syn = Integer.parseInt(arrayIds[0]);
+    private class HypFileProcessor implements FileProcessor {
+        @Override
+        public void processLine(String strLine, int lineNumber) {
+            String[] arrayIds = strLine.split(",");
+            validateParts(strLine, arrayIds, 1);
+            int syn = Integer.parseInt(arrayIds[0]);
 
-			for (int i = 1; i < arrayIds.length; i++) {
-				int hyp = Integer.parseInt(arrayIds[i]);
-				digraph.addEdge(syn, hyp);
-			}
-		}
+            for (int i = 1; i < arrayIds.length; i++) {
+                int hyp = Integer.parseInt(arrayIds[i]);
+                digraph.addEdge(syn, hyp);
+            }
+        }
 
-		@Override
-		public void postLineProcess(int processedLines) {
-		}
-	}
+        @Override
+        public void postLineProcess(int processedLines) {
+        }
+    }
 
-	private void validateParts(String strLine, String[] parts, int minSize) {
-		if (parts == null || parts.length < minSize) {
-			throw new IllegalArgumentException("Line " + strLine + " does not have the expected format");
-		}
-	}
+    private void validateParts(String strLine, String[] parts, int minSize) {
+        if (parts == null || parts.length < minSize) {
+            throw new IllegalArgumentException("Line " + strLine
+                    + " does not have the expected format");
+        }
+    }
 
-	private void validate(String idStr, String strLine, int vertexCounter) {
-		if (!idStr.trim().equalsIgnoreCase(String.valueOf(vertexCounter))) {
-			throw new IllegalArgumentException("Line " + strLine + " does not have the expected format: expected id " + vertexCounter
-					+ " but was: " + idStr);
-		}
-	}
+    private void validate(String idStr, String strLine, int vertexCounter) {
+        if (!idStr.trim().equalsIgnoreCase(String.valueOf(vertexCounter))) {
+            throw new IllegalArgumentException("Line " + strLine
+                    + " does not have the expected format: expected id "
+                    + vertexCounter + " but was: " + idStr);
+        }
+    }
 
-	private void addNouns(int id, String nounsStr) {
-		String[] nouns = nounsStr.trim().split(" ");
+    private void addNouns(int id, String nounsStr) {
+        String[] nouns = nounsStr.trim().split(" ");
 
-		for (String noun : nouns) {
-			if(!nounToVertex.containsKey(noun)){
-				nounToVertex.put(noun, new ArrayList<Integer>());
-			}
-			nounToVertex.get(noun).add(id);
-		}
-	}
+        for (String noun : nouns) {
+            if (!nounToVertex.containsKey(noun)) {
+                nounToVertex.put(noun, new ArrayList<Integer>());
+            }
+            nounToVertex.get(noun).add(id);
+        }
+    }
 
-	private void validatGraphIsRootedDAG() {
-		boolean[] visited = new boolean[digraph.V()];
-		Set<Integer> inPath = new HashSet<>();
-		for (int i = 0; i < digraph.V(); i++) {
-			if (!visited[i]) {
-				validationDfs(i, visited, inPath);
-			}
-		}
-	}
+    private void validatGraphIsRootedDAG() {
+        boolean[] visited = new boolean[digraph.V()];
+        Set<Integer> inPath = new HashSet<>();
+        for (int i = 0; i < digraph.V(); i++) {
+            if (!visited[i]) {
+                validationDfs(i, visited, inPath);
+            }
+        }
+    }
 
-	private void validationDfs(int visit, boolean[] visited, Set<Integer> inPath) {
-		if (!visited[visit]) {
-			checkRoot(visit);
-			checkCycle(visit, inPath);
+    private void validationDfs(int visit, boolean[] visited, Set<Integer> inPath) {
+        if (!visited[visit]) {
+            checkRoot(visit);
+            checkCycle(visit, inPath);
 
-			visited[visit] = true;
-			inPath.add(visit);
+            visited[visit] = true;
+            inPath.add(visit);
 
-			Iterable<Integer> neighbors = digraph.adj(visit);
-			if (neighbors.iterator().hasNext()) {
-				validationDfs(visit, visited, inPath);
-			}
-			inPath.remove(visit);
-		}
-	}
+            Iterable<Integer> neighbors = digraph.adj(visit);
+            if (neighbors.iterator().hasNext()) {
+                validationDfs(visit, visited, inPath);
+            }
+            inPath.remove(visit);
+        }
+    }
 
-	private void checkCycle(int visit, Set<Integer> inPath) {
-		if (inPath.contains(visit)) {
-			throw new IllegalArgumentException("The has cycle: " + inPath + ", " + visit);
-		}
-	}
+    private void checkCycle(int visit, Set<Integer> inPath) {
+        if (inPath.contains(visit)) {
+            throw new IllegalArgumentException("The has cycle: " + inPath
+                    + ", " + visit);
+        }
+    }
 
-	private void checkRoot(int visit) {
-		Iterable<Integer> neighbors = digraph.adj(visit);
-		boolean isRoot = !neighbors.iterator().hasNext();
-		if (isRoot) {
-			if (root == UNKNOWN_ROOT) {
-				root = visit;
-			} else {
-				throw new IllegalArgumentException("The graph is not rooted: there are two vertex with no connection: " + root + " and "
-						+ visit);
-			}
-		}
+    private void checkRoot(int visit) {
+        Iterable<Integer> neighbors = digraph.adj(visit);
+        boolean isRoot = !neighbors.iterator().hasNext();
+        if (isRoot) {
+            if (root == UNKNOWN_ROOT) {
+                root = visit;
+            } else {
+                throw new IllegalArgumentException(
+                        "The graph is not rooted: there are two vertex with no connection: "
+                                + root + " and " + visit);
+            }
+        }
 
-	}
+    }
 
-	public Iterable<String> nouns() {
-		return nounToVertex.keySet();
-	}
+    public Iterable<String> nouns() {
+        return nounToVertex.keySet();
+    }
 
-	public boolean isNoun(String string) {
-		return nounToVertex.containsKey(string);
-	}
+    public boolean isNoun(String string) {
+        return nounToVertex.containsKey(string);
+    }
 
-	public String sap(String nounA, String nounB) {
-		List<Integer> v = nounToVertex.get(nounA);
-		List<Integer> w = nounToVertex.get(nounB);
-		SAP sap = new SAP(digraph);
-		
-		Integer ancestor = sap.ancestor(v, w);
-		
-		return synsets.get(ancestor);
-	}
-	
-	public int distance(String nounA, String nounB){
-		List<Integer> v = nounToVertex.get(nounA);
-		List<Integer> w = nounToVertex.get(nounB);
-		SAP sap = new SAP(digraph);
-		
-		return sap.length(v, w);
-	}
+    public String sap(String nounA, String nounB) {
+        List<Integer> v = nounToVertex.get(nounA);
+        List<Integer> w = nounToVertex.get(nounB);
+        SAP sap = new SAP(digraph);
+
+        int ancestor = sap.ancestor(v, w);
+
+        return synsets.get(ancestor);
+    }
+
+    public int distance(String nounA, String nounB) {
+        List<Integer> v = nounToVertex.get(nounA);
+        List<Integer> w = nounToVertex.get(nounB);
+        SAP sap = new SAP(digraph);
+
+        return sap.length(v, w);
+    }
 
 }
